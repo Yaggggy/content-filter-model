064a006fae5689db3091845a1cf42865c2596bea
Yagyansh Singh Deshwal <yagyansh.singh.cse.2022@miet.ac.in>
2025-01-31 15:33:37 +0530

    Update cisi_corpus.py

--- content_filtering_model/information-retrieval-system-main/src/corpus/cisi_corpus.py
+++ content_filtering_model/information-retrieval-system-main/src/corpus/cisi_corpus.py
@@ -2,6 +2,5 @@ from corpus.cran_corpus import CranCorpusAnalyzer
 
 
 class CisiCorpusAnalyzer(CranCorpusAnalyzer):
-    """Cisi corpus has a similar structure of the Cran Corpus"""
     def __init__(self, corpus_path, *, name='cisi', stemming=False):
         super().__init__(corpus_path, name=name, stemming=stemming)
